cfea95fbcaa5e6acadb66474d5a8d0b5e35fada8
Yiduo Wang <yiduowang47@gmail.com>
2023-03-20 14:44:41 +1100

    Handle rodrigues to quaternion when rotation angle is 0

--- Factors/utils/rot2quat.m
+++ Factors/utils/rot2quat.m
@@ -4,7 +4,11 @@ function quat = rot2quat(rot)
 %   output - quat - The Quaternion form of the same rotation, xyzw, 4x1
 
 angle = norm(rot);
-axis = rot/angle; 
+if angle == 0
+    axis = [0; 0; 1];
+else
+    axis = rot/angle; 
+end
 
 w = cos(angle/2);
 xyz = axis*sin(angle/2);
